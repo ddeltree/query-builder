282e07a722630627dfcec1ad27169d84a68bdef3
Davi Alexandre <deltreertl@gmail.com>
2023-10-14 22:40:11 -0300

    first test cases

--- __tests__/makeQuery.test.ts
+++ __tests__/makeQuery.test.ts
@@ -1,5 +1,78 @@
 import { expect, test } from 'vitest';
+import makeQuery from '../makeQuery';
 
-test('basic config', () => {
-  expect(1).toBe(1);
+test('adding and removing nothing results in the initial query', () => {
+  const initialQuery = 'a=1&b=2&c=3';
+  const addRemArgs: AddRemArgs = [
+    [undefined, undefined],
+    [undefined, {}],
+    [{}, undefined],
+    [{}, {}],
+  ];
+  for (const [add, rem] of addRemArgs) {
+    const newQuery = makeQuery(initialQuery, {
+      add,
+      rem,
+    });
+    expect(newQuery).toBe(initialQuery);
+  }
 });
+
+test('adding and removing the same existing entry results in the initial query', () => {
+  const initialQuery = 'a=1&b=2&prop=value';
+  const newQuery = makeQuery(initialQuery, {
+    add: { prop: 'value' },
+    rem: { prop: 'value' },
+  });
+  expect(newQuery).toBe(initialQuery);
+});
+
+test('adding and removing the same inexistent entry differs from the initial query', () => {
+  const initialQuery = 'a=1&b=2';
+  const newQuery = makeQuery(initialQuery, {
+    add: { prop: 'value' },
+    rem: { prop: 'value' },
+  });
+  expect(newQuery).not.toBe(initialQuery);
+});
+
+test('removing inexistent entry does nothing', () => {
+  const initialQuery = 'a=1&b=2&c=3';
+  const newQuery = makeQuery(initialQuery, {
+    rem: { prop: 'value' },
+  });
+  expect(newQuery).toBe(initialQuery);
+});
+
+test('removing existent entry does not affect other entries', () => {
+  const initialQuery = 'a=1&b=0&b=2&c=3';
+  const newQuery = makeQuery(initialQuery, {
+    rem: {
+      b: '0',
+    },
+  });
+  expect(newQuery).toBe('a=1&b=2&c=3');
+});
+
+test('removing duplicated entry removes it only once', () => {
+  const initialQuery = 'a=1&b=2&b=2&c=3';
+  const newQuery = makeQuery(initialQuery, {
+    rem: {
+      b: '2',
+    },
+  });
+  expect(newQuery).toBe('a=1&b=2&c=3');
+});
+
+test('REMOVE_ALL (*) can remove both duplicated and unique entries', () => {
+  const initialQuery = 'a=1&b=2&b=2&c=3';
+  const newQuery = makeQuery(initialQuery, {
+    rem: {
+      b: '*',
+      c: '*',
+    },
+  });
+  expect(newQuery).toBe('a=1');
+});
+
+type AddRemArgs = (Record<string, string> | undefined)[][];
